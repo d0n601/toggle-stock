87e8c5ceea7d8dfdd9f2ec03bea19936cc6d1416
Ryan Kozak <ryan@orcuttbay.com>
2019-08-28 11:15:49 -0700

    Make work for variable products with single attribute.

--- js/toggle-stock.js
+++ js/toggle-stock.js
@@ -1,38 +1,69 @@
 jQuery(document).ready(function( $ ) {
-    var toggle_stock = $("#wp-admin-bar-toggle_product_stock");
 
-    if(toggle_stock.length) {
-        toggle_stock.click(function() {
+    var toggle_stock_simple = $("#wp-admin-bar-toggle_product_stock_simple");
+    var toggle_stock_variable = $("#wp-admin-bar-toggle_product_stock_variable");
+
+
+    if(toggle_stock_simple.length) {
+        toggle_stock_simple.click(function() {
             var pid = $('#tsock_seed').attr('data-pid');
-            toggle_product_stock(pid);
+            toggle_product_stock(pid, null);
         });
+    }    
+    else if(toggle_stock_variable.length) {
+
+        $(".tsock_seed_variable").click(function() {
+            alert('Select a product option');
+        });
+
+       $('input.variation_id').change( function(){
+
+            if( '' != $('input.variation_id').val() ) {
+
+               var_id = $('input.variation_id').val();
+
+                // Hide the generic button telling you to select a product variation.
+                $(".tsock_seed_variable").css("display", "none");
+
+                // Show the toggle button for selected variation.
+
+                // Hide other variation buttons that may have been displayed from previous selections. 
+                $(".tsock_seed_variation").css("display", "none");
+
+                // Display button for selected variation
+                $("*[data-variation="+var_id+"]").css("display", "inline-block");
+
+                toggle_stock_variable.click(function() {
+                    var pid = $('#tsock_seed').attr('data-pid');
+                    toggle_product_stock(pid, var_id);
+                });
+            }
+         });
     }
 });
 
 
-
-function toggle_product_stock(pid) {
+function toggle_product_stock(pid, var_id) {
 
     var url = "/wp-json/toggle-stock/v1/toggle";
 
-    var json = {"pid": pid};
+    var json = {"pid": pid, "var_id": var_id};
 
-    jQuery.ajax(
-        {
-            url: url,
-            type: 'POST',
-            beforeSend: function ( xhr ) {
-                xhr.setRequestHeader( 'X-WP-Nonce', wpApiSettings.nonce );
-            },
-            dataType: 'json',
-            data: json,
-            success: function (data) {
-                console.log(data);
-                document.location.reload();
-            },
-            // If something gone wrong'd
-            error: function (errorThrown) {
-                console.log(errorThrown);
-            }
-        });
+    jQuery.ajax( {
+        url: url,
+        type: 'POST',
+        beforeSend: function ( xhr ) {
+            xhr.setRequestHeader( 'X-WP-Nonce', wpApiSettings.nonce );
+        },
+        dataType: 'json',
+        data: json,
+        success: function (data) {
+            console.log(data);
+            document.location.reload();
+        },
+        // If something gone wrong'd
+        error: function (errorThrown) {
+            console.log(errorThrown);
+        }
+    });
 }
--- toggle-stock.php
+++ toggle-stock.php
@@ -4,7 +4,7 @@ Plugin Name: Toggle Stock
 Plugin URI: https://github.com/d0n601/toggle-stock/
 Description: Adds an "out of stock" or "in stock" button to wordpress admin panel on WooCommerce product pages, for quick toggling by shop managers.
 Author: Ryan Kozak
-Version: 0.0.2
+Version: 1.0.0
 Author URI: https://theseedgroup.com
 */
 
@@ -17,11 +17,9 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
  *
  */
 function toggle_stock_assets() {
-    
     wp_enqueue_script( 'toggle_stock_js',  plugins_url( '/js/toggle-stock.js', __FILE__ ), [ 'wp-api' ]);
     wp_register_style('toggle_stock_css', plugin_dir_url(__FILE__).'/css/toggle_stock.css');
     wp_enqueue_style('toggle_stock_css');
-
 } add_action('wp_enqueue_scripts', 'toggle_stock_assets');
 
 
@@ -70,16 +68,37 @@ function toggle_product_stock() {
 
     if(!$product) return "No Product!";
 
-    if($product->is_in_stock()) {
-        $product->set_stock_status('outofstock');
-    }
-    else {
-        $product->set_stock_status('instock');
+    // For simple products
+    if($product->is_type( 'simple')) {
+
+        if($product->is_in_stock()) {
+            $product->set_stock_status('outofstock');
+        }
+        else {
+            $product->set_stock_status('instock');
+        }
+
+
+        $product->save();
+
+        return $product;
     }
 
-    $product->save();
+    elseif($product->is_type( 'variable')) {
 
-    return $product;
+        $variation = wc_get_product($_POST['var_id']);
+
+        if($variation->is_in_stock()) {
+            $variation->set_stock_status('outofstock');
+        }
+        else {
+            $variation->set_stock_status('instock');
+        }
+
+        $variation->save();
+
+        return $variation;
+    }
 }
 
 
@@ -101,13 +120,35 @@ function toggle_product_stock_link( $wp_admin_bar ) {
 
     $pid = $product->get_id();
 
-    if($product->is_in_stock()) {
-        $button_html = "<p id='tsock_seed' class='stock out-of-stock tsock_seed_out' style='margin-top:-32px; cursor:pointer;' data-pid='$pid'>Set to Out Stock</p>";
-    }
-    else {
-        $button_html = "<p id='tsock_seed' class='stock in-stock tsock_seed_in' style='margin-top:-32px; cursor:pointer;' data-pid='$pid'>Set to In Stock</p>";
-    }
+    if($product->is_type( 'simple')) {
 
-    $wp_admin_bar->add_node( array('id' => 'toggle_product_stock', 'meta'  => array('html'  => $button_html) ));
+        if($product->is_in_stock()) {
+            $button_html = "<p id='tsock_seed' class='stock out-of-stock tsock_seed_out' data-pid='$pid'>Set to Out Stock</p>";
+        }
+        else {
+            $button_html = "<p id='tsock_seed' class='stock in-stock tsock_seed_in' data-pid='$pid'>Set to In Stock</p>";
+        }
+        $wp_admin_bar->add_node( array('id' => 'toggle_product_stock_simple', 'meta'  => array('html'  => $button_html) ));
+    }
+    elseif($product->is_type( 'variable')) {
+
+        $button_html = "<p id='tsock_seed' class='tsock_seed_variable' data-pid='$pid'>Select Variation</p>";
+
+        $all_variations = $product->get_available_variations();
+
+        foreach($all_variations as $variation) {
+
+            $var_id = $variation['variation_id'];
+
+            if($variation['is_in_stock']) {
+                $button_html .= "<p id='tsock_seed_variation' class='stock out-of-stock tsock_seed_out tsock_seed_variation' data-pid='$pid' data-variation='$var_id'>Set to Out Stock</p>";
+            }
+            else {
+                $button_html .= "<p id='tsock_seed_variation' class='stock in-stock tsock_seed_in tsock_seed_variation' data-pid='$pid' data-variation='$var_id'>Set to In Stock</p>";
+            }
+        }
+
+        $wp_admin_bar->add_node( array('id' => 'toggle_product_stock_variable', 'meta'  => array('html'  => $button_html) ));
+    }
 
 } add_action( 'admin_bar_menu', 'toggle_product_stock_link', 999 );
